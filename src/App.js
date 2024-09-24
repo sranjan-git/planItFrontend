606151f20ddd04606ce55bb7b1ba228e4232fe67
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-09-24 16:33:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import React, { useState } from "react";
+// new
+import React from "react";
 import MyCalendar from "./components/Calendar";
 import NavBar from "./components/Navbar"; 
 import Home from "./components/Home"; 
 import { Container } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { UserProvider } from "./context/ct"; // Import UserProvider
 
 const App = () => {
-  const [user, setUser] = useState(null);
-
   return (
-    <Router>
-      {/* Navbar component will always be visible */}
-      <NavBar user={user} setUser={setUser} />
+    <UserProvider>
+      <Router>
+        {/* Navbar component will always be visible */}
+        <NavBar />
 
-      {/* Routes will handle page navigation */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        {/* Routes will handle page navigation */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
 
-      {/* Calendar is conditionally rendered if the user is logged in */}
-      <Container>
-        {user ? <MyCalendar user={user} /> : null}
-      </Container>
-    </Router>
+        {/* Calendar is conditionally rendered if the user is logged in */}
+        <Container>
+          <MyCalendar />
+        </Container>
+      </Router>
+    </UserProvider>
   );
 };
 
 export default App;
+
+
+
+// old
+// import React, { useState } from "react";
+// import MyCalendar from "./components/Calendar";
+// import NavBar from "./components/Navbar"; 
+// import Home from "./components/Home"; 
+// import { Container } from "@mui/material";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+
+// const App = () => {
+//   const [user, setUser] = useState(null);
+
+//   return (
+//     <Router>
+//       {/* Navbar component will always be visible */}
+//       <NavBar user={user} setUser={setUser} />
+
+//       {/* Routes will handle page navigation */}
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+
+//       {/* Calendar is conditionally rendered if the user is logged in */}
+//       <Container>
+//         {user ? <MyCalendar user={user} /> : null}
+//       </Container>
+//     </Router>
+//   );
+// };
+
+// export default App;
